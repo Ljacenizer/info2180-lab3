a9ebcb56752d6149df454d69ea74f0b17555f569
ljacenizer <jasonmcleggo@gmail.com>
2021-10-10 16:09:40 -0400

    number 2 completed :T

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,28 @@
-/*var divsquare = document.querySelector('div').classList('.square');
-divsquare.setAttribute('id', '.square');
-*/
 window.onload = function(){
-    var divsquare = document.getElementById("board");
-    for(let i=0; i<divsquare.children.length;i++){
-        divsquare.children[i].setAttribute("class","square");
+    var divsquare = document.querySelectorAll("#board > div");
+    var currplayer = true;
+    var playerX = "X";
+    var playerO = "O";
+
+    for(let i=0; i<=8; i++){
+        divsquare[i].setAttribute("class","square");
+        divsquare[i].innerHTML = "";
     }
 
+    divsquare.forEach((element) =>{
+        element.classList.add("square");
+        element.innerHTML = "";
+        element.addEventListener("click", function(){
+            if ((currplayer == true) && (element.innerHTML == "")){
+                element.innerHTML = playerX;
+                element.classList.add("X");
+                currplayer = false;
+            }
+            else if ((currplayer == false) && (element.innerHTML =="")){
+                element.innerHTML = playerO;
+                element.classList.add("O");
+                currplayer = true;
+            }
+        });
+    });
 }
